1b7e6d5ea518df7a819eed84385793b48e2944c1
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-02-13 17:01:08 +0100

    server: add /notes endpoint, "fix" cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const PORT = 3001;
 
 const server = express();
 
-server.get("/", () => {
-    console.log("GET on / !!!");
+const notes = ["Hello", "Note"];
+
+server.use(cors());
+
+server.get("/", (req, res) => {
+    res.send("Hello Notes App");
+});
+
+server.get("/notes", (req, res) => {
+    res.send(notes);
 });
 
 server.listen(PORT, () => {

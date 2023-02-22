54318c2d0c934acf93c3a5c5f9aa8142ce7bb423
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-02-22 16:19:34 +0100

    client: Add delete note button

--- client/src/components/NoteList.jsx
+++ client/src/components/NoteList.jsx
@@ -1,8 +1,25 @@
 export default function NoteList({ notes }) {
+    function deleteNote(index) {
+        fetch(`http://localhost:3001/notes/${index}`, {
+            method: "DELETE",
+        });
+    }
+
     return (
         <ul>
             {notes.map((note, i) => {
-                return <li key={`${i}-${note}`}>{note}</li>;
+                return (
+                    <li key={`${i}-${note}`}>
+                        {note}
+
+                        <button
+                            title="Delete note"
+                            onClick={() => deleteNote(i)}
+                        >
+                            X
+                        </button>
+                    </li>
+                );
             })}
         </ul>
     );
